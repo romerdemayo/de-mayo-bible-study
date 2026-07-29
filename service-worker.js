0704a3422801ecbfbe6c4c938d91d097428fc730
Romer De Mayo <romer.demayo@gmail.com>
2026-07-30 10:36:37 +1200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 /* De Mayo Bible Ministry | Version 86 Bilingual Responsive Presentations */
-const CACHE='de-mayo-bible-v108-auto-reading-plans';
+const CACHE='de-mayo-bible-v110-data-protection';
 const OFFLINE_URL = './index.html';
 const ASSETS = [
   './','./index.html','./styles.css','./app.js','./bible-data.js','./analytics-config.js','./analytics-loader.js',
